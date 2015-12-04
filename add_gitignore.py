4845a45b80e96118138eb0ab19f42b936c17f41f
Dhruv Mishra <ubiquitiousdm@gmail.com>
2015-12-04 22:56:54 +0530

    missed slash in directory

--- add_gitignore.py
+++ add_gitignore.py
@@ -3,7 +3,7 @@ import os
 def add_to_global():
     for file in os.listdir('./'):
         if file.__contains__('.gitignore'):
-            exec_command = 'git config --global core.excludesfile ' + os.getcwd() + file
+            exec_command = 'git config --global core.excludesfile ' + os.getcwd() + '/'+file
             print exec_command
             os.system(exec_command)
 
